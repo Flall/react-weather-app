3675c637157e38bec90a80ee104cb70e9bfae44a
Flall <119894671+Flall@users.noreply.github.com>
2023-03-11 17:38:40 +0100

    Add H1 and footer and link to GitHub

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Francesca Lalli and is
+        <a
+          href="https://github.com/Flall/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
