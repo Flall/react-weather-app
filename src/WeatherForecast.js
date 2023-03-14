5552063a1ae26179113baff6aa07d0a26054fd89
Flall <119894671+Flall@users.noreply.github.com>
2023-03-14 19:29:51 +0100

    WeatherForecast component created

---
+++ src/WeatherForecast.js
@@ -0,0 +1,68 @@
+import React from "react";
+import WeatherInfo from "./WeatherInfo";
+import Weather from "./Weather";
+import "./WeatherForecast.css";
+
+export default function WeatherForecast(props) {
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="col">
+          <div className="WeatherForecast-day">Thu</div>
+          <img
+            src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+            className="float-left"
+          />
+          <div className="WeatherForecast-temp">
+            <span className="WeatherForecast-temp-max">19°</span> |{" "}
+            <span className="WeatherForecast-temp-min">10°</span>
+          </div>
+        </div>
+        <div className="col">
+          <div className="WeatherForecast-day">Thu</div>
+          <img
+            src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+            className="float-left"
+          />
+          <div className="WeatherForecast-temp">
+            <span className="WeatherForecast-temp-max">19°</span> |{" "}
+            <span className="WeatherForecast-temp-min">10°</span>
+          </div>
+        </div>
+        <div className="col">
+          <div className="WeatherForecast-day">Thu</div>
+          <img
+            src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+            className="float-left"
+          />
+          <div className="WeatherForecast-temp">
+            <span className="WeatherForecast-temp-max">19°</span> |{" "}
+            <span className="WeatherForecast-temp-min">10°</span>
+          </div>
+        </div>
+        <div className="col">
+          <div className="WeatherForecast-day">Thu</div>
+          <img
+            src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+            className="float-left"
+          />
+          <div className="WeatherForecast-temp">
+            <span className="WeatherForecast-temp-max">19°</span> |{" "}
+            <span className="WeatherForecast-temp-min">10°</span>
+          </div>
+        </div>
+        <div className="col">
+          <div className="WeatherForecast-day">Thu</div>
+          <img
+            src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+            className="float-left"
+          />
+          <div className="WeatherForecast-temp">
+            <span className="WeatherForecast-temp-max">19°</span> |{" "}
+            <span className="WeatherForecast-temp-min">10°</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
